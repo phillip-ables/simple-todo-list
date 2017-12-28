f83013655cf49f9e88080b6f22b0e1f851f38a7b
philables <phillipables1@gmail.com>
2017-12-28 01:31:49 -0500

    alerts users input on enter

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ console.log("main.js is here");
 document.querySelector('#input-text').addEventListener('keypress', function (e) {
 	var key = e.which || e.keyCode;
 	if (key == 13) {
-		alert("the enter key has been keyed!!");
+		// let the games beggin
+		console.log("the enter key has been keyed!!");
+
+		var userText = document.getElementById('input-text');
+		alert(userText.value)
+
 	}
 })
